65dc4be8ae9fa67493da578833496e964d5d111f
Yuliia Marych <yulyamarich@icloud.com>
2022-07-06 14:35:32 +0300

    initial commit

---
+++ SpaceX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by Julia on 06.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
